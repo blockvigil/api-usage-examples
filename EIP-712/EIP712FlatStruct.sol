7a213b93a7e6b44463142019db4f761b3394d716
anomit ghosh <anomit.ghosh@gmail.com>
2019-10-07 12:06:07 +0530

    Adding example for EIP-712 with a flat structure with no nested structures

---
+++ EIP-712/EIP712FlatStruct.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+contract EIP712FlatExample {
+    uint256 constant chainId = 8995;
+
+    struct Unit {
+        string actionType;
+        uint256 timestamp;
+        string authorizer;
+    }
+    /* if chainId is not a constant and instead dynamically initialized,
+     * the hash calculation seems to be off and ecrecover() returns an unexpected signing address
+
+    // uint256 internal chainId;
+    // constructor(uint256 _chainId) public{
+    //     chainId = _chainId;
+    // }
+
+    */
+
+    // EIP-712 boilerplate begins
+    event SignatureExtracted(address indexed signer, string action);
+
+    string private constant EIP712_DOMAIN  = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+    string private constant UNIT_TYPE = "Unit(string actionType,uint256 timestamp,string authorizer)";
+
+    // type hashes. Hash of the following strings:
+    // 1. EIP712 Domain separator.
+    // 2. string describing identity type
+    // 3. string describing message type (enclosed identity type description included in the string)
+
+    bytes32 private constant EIP712_DOMAIN_TYPEHASH = keccak256(abi.encodePacked(EIP712_DOMAIN));
+    bytes32 private constant UNIT_TYPEHASH = keccak256(abi.encodePacked(UNIT_TYPE));
+
+    bytes32 private DOMAIN_SEPARATOR = keccak256(abi.encode(
+        EIP712_DOMAIN_TYPEHASH,
+        keccak256("VerifierApp101"),  // string name
+        keccak256("1"),  // string version
+        chainId,  // uint256 chainId
+        0x8c1eD7e19abAa9f23c476dA86Dc1577F1Ef401f5  // address verifyingContract
+    ));
+
+
+     // functions to generate hash representation of the struct objects
+
+    function hashUnit(Unit memory unitobj) private view returns (bytes32) {
+        return keccak256(abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(
+                    UNIT_TYPEHASH,
+                    keccak256(bytes(unitobj.actionType)),
+                    unitobj.timestamp,
+                    keccak256(bytes(unitobj.authorizer))
+                ))
+            ));
+    }
+
+    function submitProof(Unit memory _msg, bytes32 sigR, bytes32 sigS, uint8 sigV) public {
+        address recovered_signer = ecrecover(hashUnit(_msg), sigV, sigR, sigS);
+        emit SignatureExtracted(recovered_signer, _msg.actionType);
+
+    }
+
+    function verify() public view returns (bool) {
+
+        Unit memory _msgobj = Unit({
+           actionType: 'Action7440',
+           timestamp: 1570112162,
+           authorizer: 'auth239430'
+        });
+
+        bytes32 sigR = 0xc337dae0b30eff41db07f183b95e7d1241034241106b8e75cbaa83fddc0b7237;
+        bytes32 sigS = 0x4c1e71ada236d1e34ca22b89ef51ce568d29b01633290b4b28efc1375c61765e;
+        uint8 sigV = 28;
+
+        address signer = 0x00EAd698A5C3c72D5a28429E9E6D6c076c086997;
+
+        return signer == ecrecover(hashUnit(_msgobj), sigV, sigR, sigS);
+    }
+}
---
+++ EIP-712/submit_proof_flat.py
@@ -0,0 +1,125 @@
+import requests
+import json
+from dynaconf import settings
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.httpclient
+import tornado.escape
+from tornado.options import define, options
+import logging
+import sys
+
+define("port", default=6635, help="run on the given port", type=int)
+
+tornado_logger = logging.getLogger('EIP712ProofLogger')
+tornado_logger.propagate = False
+tornado_logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(u"%(levelname)-8s %(name)-4s %(asctime)s,%(msecs)d %(module)s-%(funcName)s: %(message)s")
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+stderr_handler = logging.StreamHandler(sys.stderr)
+stderr_handler.setLevel(logging.ERROR)
+stderr_handler.setFormatter(formatter)
+
+tornado_logger.addHandler(stdout_handler)
+tornado_logger.addHandler(stderr_handler)
+#
+# hn = logging.NullHandler()
+# hn.setLevel(logging.DEBUG)
+# logging.getLogger("tornado.access").addHandler(hn)
+# logging.getLogger("tornado.access").propagate = False
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with, Content-Type")
+        self.set_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+
+    def put(self, *args, **kwargs):
+        self.set_status(204)
+        self.flush()
+
+    def delete(self, *args, **kwargs):
+        self.set_status(204)
+        self.flush()
+
+    def options(self):
+        self.set_status(204)
+        self.flush()
+
+
+class FlatStructHandler(BaseHandler):
+    async def post(self):
+        # tornado_logger.debug(self.request.headers['content-type'])
+        # tornado_logger.debug('---Transaction---')
+        tornado_logger.debug(self.request.body)
+        request_json = tornado.escape.json_decode(self.request.body)
+        # self.set_status(status_code=202)
+        # self.write({'success': True})
+        command = request_json['command']
+        contract_address = request_json['contractAddress']
+        if command == 'submitApproval':
+            api_key = settings['ETHVIGIL_API_KEY']
+            # expand the message object into individual components
+            # msg_obj = ["Action7440", 1570112162, "auth239430"]
+            # msg_obj_request_str = '["Action7440", 1570112162, "auth239430"]'
+            msg_obj = list(request_json['messageObject'].values())
+            msg_obj_request_str = json.dumps(msg_obj)
+            # sig_r = "0xc337dae0b30eff41db07f183b95e7d1241034241106b8e75cbaa83fddc0b7237"
+            sig_r = request_json['sigR']
+            # sig_s = "0x4c1e71ada236d1e34ca22b89ef51ce568d29b01633290b4b28efc1375c61765e"
+            sig_s = request_json['sigS']
+            # sig_v = 28
+            sig_v = request_json['sigV']
+            headers = {'accept': 'application/json', 'Content-Type': 'application/json', 'X-API-KEY': api_key}
+            method_api_endpoint = f'{settings["REST_API_ENDPOINT"]}/contract/{contract_address}/submitProof'
+            method_args = {
+                '_msg': msg_obj_request_str,
+                'sigR': sig_r,
+                'sigS': sig_s,
+                'sigV': sig_v
+            }
+            tornado_logger.debug('Sending method args to submitApproval')
+            tornado_logger.debug(method_args)
+            r = requests.post(url=method_api_endpoint, json=method_args, headers=headers)
+            tornado_logger.debug(r.text)
+            self.set_status(status_code=r.status_code)
+            self.write(r.json())
+
+
+class WebhookHandler(BaseHandler):
+    async def post(self):
+        self.set_status(status_code=202)
+        self.write({'success': True})
+        request_json = tornado.escape.json_decode(self.request.body)
+        tornado_logger.debug('*WEBHOOK ENDPOINT*')
+        tornado_logger.debug('======Event data payload received======')
+        tornado_logger.debug(request_json)
+
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r"/flat", FlatStructHandler),
+        (r"/webhook", WebhookHandler),
+        # (r"/nested", NestedStructHandler)
+
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    try:
+        tornado.ioloop.IOLoop.current().start()
+    except KeyboardInterrupt:
+        tornado_logger.debug('Shutting down...')
+        tornado.ioloop.IOLoop.current().stop()
+
+
+if __name__ == '__main__':
+    main()
